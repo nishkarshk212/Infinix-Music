1423753cc1cac1f01a08f89d6c9d1d977c5293a5
- ⋏ Ł ꪮ ⲛ 𝛆 /𝟑 <mralone3536@gmail.com>
2025-05-29 16:07:38 +0530

    𝐀ʟᴏɴᴇ 𝐂ᴏᴅᴇʀ✨

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ SONG_DOWNLOAD_DURATION = int(
 
 LOGGER_ID = int(getenv("LOGGER_ID", ""))
 
-OWNER_ID = int(getenv("OWNER_ID", "7552579717"))
+OWNER_ID = int(getenv("OWNER_ID", "6079943111"))
 
 OWNER_USERNAME = getenv("OWNER_USERNAME","AloneHuVai")
 
@@ -45,7 +45,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AloneUpdates")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneXSupport")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AloneBotSupport")
 
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 AUTO_SUGGESTION_MODE = getenv("AUTO_SUGGESTION_MODE", "True")
