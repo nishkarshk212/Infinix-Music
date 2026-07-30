e1e5227335673998fd2b019b1928e3122f2a99c7
Nishkarsh Kumar <nishkarshkumar@Nishkarshs-MacBook-Air.local>
2026-07-30 06:09:23 +0530

    Fix ImportError: remove thumb from Infinix import block in core calls.py

--- Infinix/core/calls.py
+++ Infinix/core/calls.py
@@ -16,7 +16,7 @@ from pytgcalls import PyTgCalls, exceptions, types
 from pytgcalls.pytgcalls_session import PyTgCallsSession
 
 from Infinix import (app, config, db, lang, logger,
-                   queue, thumb, userbot, yt)
+                   queue, userbot, yt)
 from Infinix.core.youtube import YouTube, set_dl_context
 from Infinix.helpers import Media, Track, buttons, thumb, utils
 
